fd8e3abcc839871892470d09a95fa86aa2bb620e
NursultanFront <nursultan020395@gmail.com>
2023-05-23 14:34:09 +0500

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import Header from "./components/header/Header";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
+import HomePage from "./page/HomePage";
+import About from "./page/About";
+import User from "./page/User";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user:id" element={<User />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
